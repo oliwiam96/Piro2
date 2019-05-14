56dcb34bb2b8f542b107e318099ed5e4aee38bdc
xphoenix102 <xphoenix102@gmail.com>
2019-05-14 13:39:03 +0200

    Wycinanie poziomych fragnemtów wg linii

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ dil = cv2.erode(ero, kernel, iterations=2)
 
 
 # Wykrywanie linii
-lines = cv2.HoughLinesP(dil,1,np.pi/180,80,minLineLength=400,maxLineGap=10)
+lines = cv2.HoughLinesP(dil,1,np.pi/180,80,minLineLength=400,maxLineGap=20)
 
 # Pozostawienie tylko linii poziomych
 lines = list(filter(lambda l: abs(l[0][1]-l[0][3]) < 20,lines))
@@ -39,15 +39,19 @@ ldiff = list(map(lambda l: l[0]-l[1],l3))
 
 # Pozostawienie linii, które są oddalone o więcej niż 10 px
 linesFiltered = []
+dist_between_lines = 0
 for i,d in enumerate(ldiff):
-    if d>10:
+    if d>20:
+        dist_between_lines+=d
         #print(d, linesSorted[i])
         linesFiltered.append(linesSorted[i])
 
+#dist_between_lines= int(dist_between_lines/len(linesSorted))
+dist_between_lines =  50
 
-for line in linesFiltered:
-    for x1,y1,x2,y2 in line:
-        cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
+# for line in linesFiltered:
+#     for x1,y1,x2,y2 in line:
+#         cv2.line(img,(x1,y1),(x2,y2),(0,255,0),2)
 
 
 # Wykrywanie lini 2
@@ -68,5 +72,24 @@ for line in linesFiltered:
 
 
 cv2.imshow("a",img)
-#cv2.imwrite("a.jpg",img)
+        #cv2.imwrite("a.jpg",img)
 cv2.waitKey(0)
+
+for line in linesFiltered:
+    for x1,y1,x2,y2 in line:
+        # Obszar obrazka 65px powyżej linii oraz 15px poniżej
+        roi_from = max(0,y1-dist_between_lines-15)
+        roi = img[roi_from:y1+15,:]
+
+        # To jeszcze nie działa
+        edges_roi = cv2.Canny(img[roi_from:y1+15,:],220,300,apertureSize = 3)
+        im2, contours, hierarchy = cv2.findContours(edges_roi, 1, 2)
+        edges_roi = cv2.dilate(edges_roi,kernel2, iterations=2)
+        if len(contours)>0:
+            cnt = contours[0]
+            x, y, w, h = cv2.boundingRect(cnt)
+            cv2.rectangle(roi, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+        cv2.imshow("a",roi)
+        #cv2.imwrite("a.jpg",img)
+        cv2.waitKey(0)
